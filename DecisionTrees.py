70c23feb72d4b06cb704c7e7dfb2d2e02115c682
Marius <marius.macioch@rwth-aachen.de>
2023-03-20 20:58:06 +0100

    Angepasste Entscheidungsbäume

--- DecisionTrees.py
+++ DecisionTrees.py
@@ -4,23 +4,12 @@ from sklearn import tree
 import graphviz
 import numpy as np
 import csv
-
+from sklearn import tree
 
 
 #import os
 #os.environ["PATH"] += os.pathsep + 'D:/lma-mma/Interpreter/Library/bin'
 
-data = load_iris()
-data.target[[10, 25, 24]]
-list(data.target_names)
-print(data)
-print(data.target_names)
-
-
-
-
-from sklearn import tree
-iris = load_iris()
 
 data = []
 with open ("Data_1440_1_168_4_24.csv") as file:
@@ -28,8 +17,11 @@ with open ("Data_1440_1_168_4_24.csv") as file:
     for row in reader:
         row_values = [float(val) for val in row]
         data.append(row_values)
-
-
+TWW = False
+if TWW == True:
+    fn = ('T_Air', 'Q_Hou_Dem', 'P_PV', 'P_EL_Dem', 'T_Sto', 'T_TWW', 'Q_TWW',  'COP_1', 'COP_2', 'T_Mean')
+else:
+    fn = ('T_Air', 'Q_Hou_Dem', 'P_PV', 'P_EL_Dem', 'T_Sto', 'COP_1', 'COP_2', 'T_Mean')
 
 y_Opti = np.loadtxt("Modes_1440_1_168_4_24.csv")
 
@@ -44,12 +36,12 @@ print (x , y)
 
 
 dot_data = tree.export_graphviz(clf, out_file=None,
-                     feature_names= ('T_Air', 'Q_Hou_Dem', 'P_PV', 'P_EL_Dem'),
+                     feature_names= fn,
                      #class_names=data,
                      filled=True, rounded=True,
                      special_characters=True)
 graph = graphviz.Source(dot_data)
 tree.plot_tree(clf)
 graph = graphviz.Source(dot_data)
-graph.render('iris')
+graph.render('Test')
 #graph.save
